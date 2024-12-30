21676a80f932df82e502a2c6e2d5d074256e063d
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2024-12-30 13:26:35 +0530

    Added color mode toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import Logo from '../assets/logo.webp';
+import { ColorModeButton } from './ui/color-mode';
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack padding={1}>
       <Image src={Logo} alt='Logo' boxSize='60px' />
       <Text>NavBar</Text>
+      <ColorModeButton margin='0 0 0 auto' />
     </HStack>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from './components/ui/provider.tsx';
+
 import App from './App.tsx';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ColorModeProvider } from './components/ui/color-mode.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider defaultTheme='light'>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>
 );
